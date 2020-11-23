865ab042e35353c6f2553c09d8435e76e17e60d0
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-23 00:20:43 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -46,10 +46,9 @@ def add_characters
          ]
       }
    }
-   epic_tragedy [:additional_characters]=[
+   epic_tragedy [:additional_characters]=
      prince={name: "Prince Escalus"} 
      apothecary={name: "Apothecary"}
-     ]
    epic_tragedy[:additional_characters]<<{name:"Prince Escalus"}
    epic_tragedy[:additional_characters]<<{name: "Apothecary"}
    epic_tragedy
