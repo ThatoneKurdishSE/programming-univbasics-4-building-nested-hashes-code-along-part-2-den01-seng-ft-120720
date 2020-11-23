d35ff191bf2d76f07211f8833f9bd5dfdc32ff90
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-23 00:21:56 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -49,7 +49,7 @@ def add_characters
    epic_tragedy [:additional_characters]=
      prince={name: "Prince Escalus"} 
      apothecary={name: "Apothecary"}
-   epic_tragedy[:additional_characters]<<{name:"Prince Escalus"}
-   epic_tragedy[:additional_characters]<<{name: "Apothecary"}
+   epic_tragedy[:additional_characters]<<prince
+  epic_tragedy[:additional_characters]<<apothecary
    epic_tragedy
 end
